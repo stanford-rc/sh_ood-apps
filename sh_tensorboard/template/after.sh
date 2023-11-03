92f76efef1fe8fa6f976ea4059925c8b8677f60f
Kilian Cavalotti <kilian@stanford.edu>
2023-11-03 16:06:22 -0700

    sh_tensorboard: minor script format

--- sh_tensorboard/template/after.sh
+++ sh_tensorboard/template/after.sh
@@ -4,8 +4,8 @@ echo "$(date): waiting for Tensorboard server to open port ${app_port}..."
 if wait_until_port_used "127.0.0.1:${port}" 300; then
     echo "$(date): discovered Tensorboard server listening on port ${app_port}!"
 
-  # wait for the authenticating proxy to start
-  echo "$(date): waiting for authrevproxy server to open port ${proxy_port}..."
+    # wait for the authenticating proxy to start
+    echo "$(date): waiting for authrevproxy server to open port ${proxy_port}..."
 
     if wait_until_port_used "${node}:${proxy_port}" 60; then
         echo "$(date): discovered authrevproxy server listening on port ${proxy_port}!"
